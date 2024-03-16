ab653f05dab7e68349db1a731816913811df4401
Franz Stecher <franz.stecher@deutschebahn.com>
2024-03-16 11:18:01 +0100

    feat: write survey in csv

--- docker/app/app.py
+++ docker/app/app.py
@@ -1,3 +1,5 @@
+import random
+import time
 from flask import Flask, render_template, jsonify, request
 from flask_cors import CORS
 
@@ -11,10 +13,11 @@ def remove_permissions_policy_header(response):
 
 @app.route('/')
 def index():
+    # TODO: server_ip dynamisch setzen
     return render_template('index.html', server_ip='http://192.168.178.171:5000', survey_heading='Umfrage 1', survey_description='Bitte beantworte die folgenden Fragen:',
         survey_text=
         '''
-        Das ist mein Beispieltext. Hier kannst du die Umfragebeschreibung hinzufügen. Lorem Ipsum dolor sit amet.
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
         '''
             )
 
@@ -25,6 +28,24 @@ def save_words():
     if words:
         # Hier kannst du die Logik hinzufügen, um die Wörter zu speichern oder weiterzuverarbeiten
         print("Empfangene Wörter:", words)
+
+        # TODO: besseren weg finden um die Datei zu benennen und eindeutig zu machen
+        # TODO: refactor in eine Funktion um den try catch block auszulagern
+        # generiere eine zufällige Zahl
+        random_number = random.randint(1, 100000000000) + time.time().as_integer_ratio()[0]
+
+        # prüfe ob die Datei schon existiert
+        try:
+            with open(f'survey-{random_number}.csv', 'x') as file:
+                file.write('word,position\n')
+        except FileExistsError:
+            random_number = random.randint(1, 100000000000) + time.time().as_integer_ratio()[0]
+            pass
+
+        # Speicher jedes wort und dessen position in csv
+        with open(f'survey-{random_number}.csv', 'a') as file:
+            for word_obj in words:
+                file.write(f'{word_obj["word"]},{word_obj["position"]}\n')
 
 
         return jsonify({'message': 'Wörter erfolgreich empfangen.'}), 200
