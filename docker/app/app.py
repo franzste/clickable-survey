7223436658fe35dcd8f804ca4c34cb790f2d515a
Franz Stecher <franz.stecher@deutschebahn.com>
2024-03-16 11:38:10 +0100

    feat: use docker mountpoints to import survey_text and export the csv results

--- docker/app/app.py
+++ docker/app/app.py
@@ -13,12 +13,12 @@ def remove_permissions_policy_header(response):
 
 @app.route('/')
 def index():
+    with open('/import/survey_text.txt', 'r') as file:
+        survey_text = file.read()
+
     # TODO: server_ip dynamisch setzen
     return render_template('index.html', server_ip='http://192.168.178.171:5000', survey_heading='Umfrage 1', survey_description='Bitte beantworte die folgenden Fragen:',
-        survey_text=
-        '''
-        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-        '''
+        survey_text=survey_text
             )
 
 @app.route('/api/words', methods=['POST'])
@@ -36,14 +36,14 @@ def save_words():
 
         # prüfe ob die Datei schon existiert
         try:
-            with open(f'survey-{random_number}.csv', 'x') as file:
+            with open(f'/export/survey-{random_number}.csv', 'x') as file:
                 file.write('word,position\n')
         except FileExistsError:
             random_number = random.randint(1, 100000000000) + time.time().as_integer_ratio()[0]
             pass
 
         # Speicher jedes wort und dessen position in csv
-        with open(f'survey-{random_number}.csv', 'a') as file:
+        with open(f'/export/survey-{random_number}.csv', 'a') as file:
             for word_obj in words:
                 file.write(f'{word_obj["word"]},{word_obj["position"]}\n')
 
